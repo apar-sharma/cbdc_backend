5f4101fa7f4aa9863b2dd052a3bade2380b3f931
apar-sharma <aparsharma321@gmail.com>
2025-01-30 22:19:40 +0545

    fiddeling around with the db connection

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return mongoose.connect(process.env.CONNECT_URL, {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 };
